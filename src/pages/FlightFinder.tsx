c1a91e4e86457178d8e03a71aca816c804bdf111
Venkata <venkata2626@gmail.com>
2022-08-02 09:41:21 -0400

    Flight Finder -Search

--- src/pages/FlightFinder.tsx
+++ src/pages/FlightFinder.tsx
@@ -1,9 +1,34 @@
+import { Autocomplete, TextField } from "@mui/material";
+
 const FlightFinder = (): JSX.Element | null => {
+  const top100Flights = [
+    { label: "John F Kennedy Intl (New York)", code: "JFK" },
+    { label: "Indira Gandhi Int'l (New Delhi)", code: "DEL" },
+    { label: "Newark Liberty Intl (Newark)", code: "EWR" },
+    { label: "LaGuardia (New York)", code: "LGA" },
+  ];
+
   return (
     <div>
       <h1>Flight Finder</h1>
-      <div>
-        <p>Search for flights by origin and destination airport</p>
+      <p>Search for flights by origin and destination airport</p>
+      <div style={{ display: "flex" }}>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={top100Flights}
+          sx={{ width: 300, marginRight: "20px" }}
+          renderInput={(params) => <TextField {...params} label="Origin" />}
+        />
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={top100Flights}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Destination" />
+          )}
+        />
       </div>
     </div>
   );

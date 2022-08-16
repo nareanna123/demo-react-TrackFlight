6a902fc769261f897004de6aee932adeeb07138a
Venkata <venkata2626@gmail.com>
2022-08-16 13:51:23 -0400

    Added new table ui from mui in the flight finder page

--- src/pages/FlightDetails.tsx
+++ src/pages/FlightDetails.tsx
@@ -1,34 +1,91 @@
-const FlightsList = (props: any) : JSX.Element | null => {
-     console.log(props.flights.length);
-   
-          return (
+import styled from "@emotion/styled";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TableSortLabel,
+  Box,
+} from "@mui/material";
+import { visuallyHidden } from "@mui/utils";
 
-                    <div> 
-                <table border={1} className="table" >
-                    <thead>
-                        <tr>
-                            <th align="left">Airline</th>
-                            <th align="left">Air Craft</th>
-                            <th align="left">Status</th>
-                            <th align="left">Departure</th>
-                            <th align="left">Arrival</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.flights.map((flight:any, index: any) => (
-                            <tr key={index}>
-                                <td align="left">{flight.airline.name}</td>
-                                <td align="left">{flight.flight.number}</td>
-                                <td align="left">{flight.flight_status}</td>
-                                <td align="left">{flight.departure.airport}</td>
-                                <td align="left">{flight.arrival.airport}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                    </table>           
-                    </div> 
-          )
-    
-  }
+const FlightsList = (props: any): JSX.Element | null => {
+  const { sort, order, orderBy } = props;
 
-export default FlightsList
+  const SortLabel = (props: any) => {
+    return (
+      <TableSortLabel
+        active={props.orderBy === props.headCell}
+        direction={props.orderBy === props.headCell ? props.order : "asc"}
+        //   onClick={handleRequestSort(headCell.id)}
+      >
+        {props.headCell.label}
+        {props.orderBy === props.headCell ? (
+          <Box component="span" sx={visuallyHidden}>
+            {props.order === "desc" ? "sorted descending" : "sorted ascending"}
+          </Box>
+        ) : null}
+      </TableSortLabel>
+    );
+  };
+
+  return (
+    <StyledContainer>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <StyledHeaderCell onClick={() => sort("airline")}>
+                Airline{" "}
+                <SortLabel order={order} orderBy={orderBy} headCell="airline" />
+              </StyledHeaderCell>
+              <TableCell align="right">Air Craft</TableCell>
+              <TableCell align="right">Status</TableCell>
+              <TableCell align="right" onClick={() => sort("departure")}>
+                Departure{" "}
+                <SortLabel
+                  order={order}
+                  orderBy={orderBy}
+                  headCell="departure"
+                />
+              </TableCell>
+              <TableCell align="right" onClick={() => sort("arrival")}>
+                Arrival{" "}
+                <SortLabel order={order} orderBy={orderBy} headCell="arrival" />
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.flights.map((flight: any, index: any) => (
+              <TableRow
+                key={index}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {flight.airline}
+                </TableCell>
+                <TableCell align="right">{flight.aircraft}</TableCell>
+                <TableCell align="right">{flight.status}</TableCell>
+                <TableCell align="right">{flight.departure}</TableCell>
+                <TableCell align="right">{flight.arrival}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </StyledContainer>
+  );
+};
+
+export default FlightsList;
+
+const StyledContainer = styled.div`
+  margin: 20px;
+`;
+
+const StyledHeaderCell = styled(TableCell)`
+  cursor: pointer;
+`;
